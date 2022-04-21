5cb76e41b91a06e890f26b8ddc57f1d37d04dbcc
unknown <elliotlee623@gmail.com>
2022-04-21 02:39:26 -0400

    add comments

--- split_script.py
+++ split_script.py
@@ -12,24 +12,31 @@ def main(spark):
     #####--------------YOUR CODE STARTS HERE--------------#####
 
     #Use this template to as much as you want for your parquet saving and optimizations!
-    #ratings = spark.read.csv(f'hdfs:/user/el3418/ml-latest-small/ratings.csv', schema='userId INT, movieId INT, rating DOUBLE, timestamp INT')
+    #schema='userId INT, movieId INT, rating DOUBLE, timestamp INT'
     ratings = spark.read.option("header",True).csv(f'hdfs:/user/el3418/ml-latest-small/ratings.csv')
     print('ratings.csv schema')
     ratings.printSchema()
 
     #There are 610 unique Ids, but the docs say 600
     print('Number of unique userIds')
+    print(len(list(ratings.select('userId').distinct().toPandas()['userId'])))
+
+    #Split uniqueIds based on index slicing
     uniqueIds = list(ratings.select('userId').distinct().toPandas()['userId'])
     train = uniqueIds[:400]
     validation = uniqueIds[400:500]
     test = uniqueIds[500:]
 
+    #Create a new column in the dataframe. Every row will have value 'train' for userId in the train list, 'validation' for userId in the validation list, and 'test' for userId in the test list
     ratings2 = ratings.withColumn("train_val_test", when(col("userId").isin(train),lit("train")).when(col("userId").isin(validation), lit("validation")).otherwise(lit("test")))
 
+    #Per Brian's suggestion in Brightspace discussion tab, training data will have 100% of reviews for users in training, 30% for users in validation, and 30% for users in test
     training_data = ratings2.sampleBy("train_val_test", fractions={'train':1, 'validation': 0.3, 'test': 0.3}, seed=1234)
 
+    #Save to HDFS
     training_data.write.csv('training_data.csv')
 
+    #Get count of results
     training_data.groupBy('train_val_test').count().orderBy('train_val_test').show()
 
 # Only enter this block if we're in main
