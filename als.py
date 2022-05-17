74f035a1bea55e6111afcb358717505458474721
el3418 <el3418@nyu.edu>
2022-05-17 05:22:53 -0400

    update als and lightfm

--- als.py
+++ als.py
@@ -4,20 +4,34 @@ from pyspark.mllib.evaluation import RegressionMetrics, RankingMetrics
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 from pyspark.sql.functions import col, mean, collect_list
+import time
+from sklearn.metrics import average_precision_score
+
+def mAP(top_k_recommendations, user_labels):
+    ap = 0
+    for movie_id in user_labels:
+        running_sum = 0
+        for rank, r in enumerate(top_k_recommendations):
+            if r == movie_id:
+                running_sum = 1 + 1/(rank+1)
+                break 
+        ap += (running_sum/len(top_k_recommendations))
+    
+    return ap/len(user_labels)
 
 def main(spark, crossValidate=False):
 
-    train = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_train_small.csv')
-    #train = train.toDF('userId', 'movieId', 'rating', 'timestamp', 'split')
-    train = train.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer'))
+    train = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_train_large.csv')
+    train = train.toDF('userId', 'movieId', 'rating', 'timestamp')
+    train = train.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer')).withColumn('rating', col('rating').cast('float'))
 
-    val = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_val_small.csv')
-    #val = val.toDF('userId', 'movieId', 'rating', 'timestamp', 'split')
-    val = val.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer'))
+    val = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_val_large.csv')
+    val = val.toDF('userId', 'movieId', 'rating', 'timestamp')
+    val = val.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer')).withColumn('rating', col('rating').cast('float'))
 
-    test = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_test_small.csv')
-    #test = test.toDF('userId', 'movieId', 'rating', 'timestamp', 'split')
-    test = test.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer'))
+    test = spark.read.option("header", True).csv(f'hdfs:/user/el3418/new_test_large.csv')
+    test = test.toDF('userId', 'movieId', 'rating', 'timestamp')
+    test = test.withColumn('userId', col('userId').cast('integer')).withColumn('movieId', col('movieId').cast('integer')).withColumn('rating', col('rating').cast('float'))
 
     als = ALS(
         rank=100,
@@ -69,13 +83,27 @@ def main(spark, crossValidate=False):
 
         nrecommendations = best_model.recommendForAllUsers(100)
     else:
+        start = time.process_time()
         model = als.fit(train)
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to fit model: ", elapsed_time)
+
+        start = time.process_time()
+        test_predictions = model.transform(test)
 
         #Output in the form user | [[array,of,movie,ids,being,recommended]]
         nrecommendations = model.recommendForAllUsers(100)
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to transform and give recommendations: ", elapsed_time)
             
     #test_predictions = model.transform(test)
+    start = time.process_time()
     RMSE = evaluator.evaluate(test_predictions)
+    end = time.process_time()
+    elapsed_time = end - start
+    print("Time to calculate RMSE: ", elapsed_time)
     print(RMSE)
 
     #Uncomment for per-movie rows
@@ -94,18 +122,15 @@ def main(spark, crossValidate=False):
     movies_list.limit(10).show()
     joined_col = movies_list.join(nrecommendations, ["userId"])
     dropped_cols = joined_col.drop("userId")
+
+    dropped_cols.show()
     
-    # define a list
-    l=[]
-
-    # convert to tuple and append to list
-    for i in dropped_cols.collect():
-        l.append(tuple(i))
-
-    predictionAndLabels = spark.sparkContext.parallelize(l)
-    metrics = RankingMetrics(predictionAndLabels)
-    print(metrics.meanAveragePrecisionAt(100))
-
+    rdd=dropped_cols.rdd.map(lambda x: 
+        (x[0],x[1],mAP(x[0],x[1]))
+        ) 
+    result_df = rdd.toDF(["movies_list", "recommendations", "precision"])
+    
+    print(result_df.select(mean('precision')).collect())
 
 if __name__ == "__main__":
 
--- extension.py
+++ extension.py
@@ -4,6 +4,8 @@ from lightfm.evaluation import precision_at_k
 from lightfm import LightFM
 import pandas as pd
 import scipy.sparse as sp
+import time
+import matplotlib.pyplot as plt
 
 def get_df_matrix_mappings(df, row_name, col_name):
     """Map entities in interactions df to row and column indices
@@ -74,6 +76,60 @@ def df_to_matrix(df, row_name, col_name):
     interactions = interactions.tocsr()
     return interactions, rid_to_idx, idx_to_rid, cid_to_idx, idx_to_cid
 
+def train_test_split(interactions, split_count, fraction=None):
+    """
+    Split recommendation data into train and test sets
+    Params
+    ------
+    interactions : scipy.sparse matrix
+        Interactions between users and items.
+    split_count : int
+        Number of user-item-interactions per user to move
+        from training to test set.
+    fractions : float
+        Fraction of users to split off some of their
+        interactions into test set. If None, then all
+        users are considered.
+    """
+    # Note: likely not the fastest way to do things below.
+    train = interactions.copy().tocoo()
+    val = sp.lil_matrix(train.shape)
+    test = sp.lil_matrix(train.shape)
+
+    if fraction:
+        try:
+            user_index = np.random.choice(
+                np.where(np.bincount(train.row) >= split_count * 2)[0],
+                replace=False,
+                size=np.int64(np.floor(fraction * train.shape[0]))
+            ).tolist()
+        except:
+            print(('Not enough users with > {} '
+                  'interactions for fraction of {}')\
+                  .format(2*split_count, fraction))
+            raise
+    else:
+        user_index = range(train.shape[0])
+
+    train = train.tolil()
+
+    for user in user_index:
+        val_test_interactions = np.random.choice(interactions.getrow(user).indices,
+                                        size=split_count,
+                                        replace=False)
+        split = np.array_split(val_test_interactions, 2)
+        val_interactions = split[0]
+        test_interactions = split[1]
+        train[user, val_test_interactions] = 0.
+        # These are just 1.0 right now
+        val[user, val_interactions] = interactions[user, val_interactions]
+        test[user, test_interactions] = interactions[user, test_interactions]
+
+
+    # Test and training are truly disjoint
+    assert(train.multiply(test).nnz == 0)
+    return train.tocsr(), val.tocsr(), test.tocsr(), user_index
+
 def subset_to_matrix(interactions, uid_to_idx, mid_to_idx, ratings, subset):
 
     diff = ratings.merge(subset, how = 'outer' ,indicator=True).loc[lambda x : x['_merge']=='left_only']
@@ -91,41 +147,92 @@ def subset_to_matrix(interactions, uid_to_idx, mid_to_idx, ratings, subset):
     return sub_mat.tocsr()
 
 def main():
-    colnames=['userId', 'movieId', 'rating', 'timestamp']
-    ratings = pd.read_csv(f'ratings_small.csv', names=colnames)
-    train_df = pd.read_csv(f'new_train_small.csv')
-    val_df = pd.read_csv(f'new_val_small.csv')
-    test_df = pd.read_csv(f'new_test_small.csv')
+    fit_time = []
+    train_time = []
+    val_time = []
+    test_time = []
+    train_map = []
+    val_map = []
+    test_map = []
 
-    train_df.reset_index(inplace=True)
+    for i in range(5):
+        colnames=['userId', 'movieId', 'rating', 'timestamp']
+        ratings = pd.read_csv(f'ratings_small.csv', names=colnames)
+        train_df = pd.read_csv(f'new_train_small.csv')
+        val_df = pd.read_csv(f'new_val_small.csv')
+        test_df = pd.read_csv(f'new_test_small.csv')
 
-    print(ratings.head())
-    print(train_df.head())
+        train_df.reset_index(inplace=True)
 
-    ratings = ratings[['userId','movieId']]
-    train_df = train_df[['userId', 'movieId']]
-    val_df = val_df[['userId', 'movieId']]
-    test_df = test_df[['userId', 'movieId']]
+        print(ratings.head())
+        print(train_df.head())
 
-    likes, uid_to_idx, idx_to_uid, mid_to_idx, idx_to_mid = df_to_matrix(ratings, 'userId', 'movieId')
+        ratings = ratings[['userId','movieId']]
+        train_df = train_df[['userId', 'movieId']]
+        val_df = val_df[['userId', 'movieId']]
+        test_df = test_df[['userId', 'movieId']]
 
-    train = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, train_df)
-    val = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, val_df)
-    test = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, test_df)
+        likes, uid_to_idx, idx_to_uid, mid_to_idx, idx_to_mid = df_to_matrix(ratings, 'userId', 'movieId')
 
-    assert(train.multiply(val).nnz == 0)
-    assert(train.multiply(test).nnz == 0)
-    assert(val.multiply(test).nnz == 0)
-    
-    model = LightFM(loss='warp')
-    # Initialize model.
-    model.fit(train, epochs=10)
-    train_precision = precision_at_k(model, train, k=100).mean()
-    val_precision = precision_at_k(model, val, k=100).mean()
-    test_precision = precision_at_k(model, test, k=100).mean()
-    print(train_precision)
-    print(val_precision)
-    print(test_precision)
+        train = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, train_df)
+        val = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, val_df)
+        test = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, test_df)
+
+        assert(train.multiply(val).nnz == 0)
+        assert(train.multiply(test).nnz == 0)
+        assert(val.multiply(test).nnz == 0)
+        
+        start = time.process_time()
+        model = LightFM(loss='warp')
+        # Initialize model.
+        model.fit(train, epochs=10)
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to fit model: ", elapsed_time)
+        fit_time.append(elapsed_time)
+
+        start = time.process_time()
+        train_precision = precision_at_k(model, train, k=100).mean()
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to calculate training precision: ", elapsed_time)
+        train_time.append(elapsed_time)
+
+        start = time.process_time()
+        val_precision = precision_at_k(model, val, k=100).mean()
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to calculate validation precision: ", elapsed_time)
+        val_time.append(elapsed_time)
+
+        start = time.process_time()
+        test_precision = precision_at_k(model, test, k=100).mean()
+        end = time.process_time()
+        elapsed_time = end - start
+        print("Time to calculate test precision: ", elapsed_time)
+        test_time.append(elapsed_time)
+
+        print(train_precision)
+        print(val_precision)
+        print(test_precision)
+        train_map.append(train_precision)
+        val_map.append(val_precision)
+        test_map.append(test_precision)
+
+    plt.plot(np.arange(5), fit_time)
+    plt.show()
+    plt.plot(np.arange(5), train_time)
+    plt.show()
+    plt.plot(np.arange(5), val_time)
+    plt.show()
+    plt.plot(np.arange(5), test_time)
+    plt.show()
+    plt.plot(np.arange(5), train_map)
+    plt.show()
+    plt.plot(np.arange(5), val_map)
+    plt.show()
+    plt.plot(np.arange(5), test_map)
+    plt.show()
 
 if __name__ == "__main__":
 
