27c2fafe855f16643c4a1ccd9665e0c388395cc0
Harshanand <34165187+iamharsha1999@users.noreply.github.com>
2022-05-17 20:59:35 -0400

    updated extension

--- extension.py
+++ extension.py
@@ -4,11 +4,11 @@ from lightfm.evaluation import precision_at_k
 from lightfm import LightFM
 import pandas as pd
 import scipy.sparse as sp
-import time
-import matplotlib.pyplot as plt
 
 def get_df_matrix_mappings(df, row_name, col_name):
-    """Map entities in interactions df to row and column indices
+    """
+    Obtain mappings of interactions between row (matrix) item and column (matrix) item. 
+
     Parameters
     ----------
     df : DataFrame
@@ -17,15 +17,18 @@ def get_df_matrix_mappings(df, row_name, col_name):
         Name of column in df which contains row entities.
     col_name : str
         Name of column in df which contains column entities.
+
     Returns
     -------
     rid_to_idx : dict
-        Maps row ID's to the row index in the eventual interactions matrix.
+        Mapping from Row IDs -> Rows Indices 
     idx_to_rid : dict
-        Reverse of rid_to_idx. Maps row index to row ID.
+        Mapping from Rows Indices -> Row IDs
     cid_to_idx : dict
-        Same as rid_to_idx but for column ID's
+        Mapping from Column IDs -> Column Indices 
     idx_to_cid : dict
+        Mapping from Column Indices -> Column IDs
+
     """
 
 
@@ -46,25 +49,29 @@ def get_df_matrix_mappings(df, row_name, col_name):
 
 
 def df_to_matrix(df, row_name, col_name):
-    """Take interactions dataframe and convert to a sparse matrix
+    """
+    Obtains the sparse matrix with interaction from the dataframe
+
     Parameters
     ----------
     df : DataFrame
     row_name : str
     col_name : str
+
     Returns
     -------
-    interactions : sparse csr matrix
+    interactions : Sparse Matrix
     rid_to_idx : dict
+        Mapping from Row IDs -> Rows Indices 
     idx_to_rid : dict
+        Mapping from Rows Indices -> Row IDs
     cid_to_idx : dict
+        Mapping from Column IDs -> Column Indices 
     idx_to_cid : dict
+        Mapping from Column Indices -> Column IDs
     """
 
-    rid_to_idx, idx_to_rid,\
-        cid_to_idx, idx_to_cid = get_df_matrix_mappings(df,
-                                                        row_name,
-                                                        col_name)
+    rid_to_idx, idx_to_rid, cid_to_idx, idx_to_cid = get_df_matrix_mappings(df, row_name, col_name)
 
     def map_ids(row, mapper):
         return mapper[row]
@@ -75,60 +82,6 @@ def df_to_matrix(df, row_name, col_name):
     interactions = sp.coo_matrix((V, (I, J)), dtype=np.float64)
     interactions = interactions.tocsr()
     return interactions, rid_to_idx, idx_to_rid, cid_to_idx, idx_to_cid
-
-def train_test_split(interactions, split_count, fraction=None):
-    """
-    Split recommendation data into train and test sets
-    Params
-    ------
-    interactions : scipy.sparse matrix
-        Interactions between users and items.
-    split_count : int
-        Number of user-item-interactions per user to move
-        from training to test set.
-    fractions : float
-        Fraction of users to split off some of their
-        interactions into test set. If None, then all
-        users are considered.
-    """
-    # Note: likely not the fastest way to do things below.
-    train = interactions.copy().tocoo()
-    val = sp.lil_matrix(train.shape)
-    test = sp.lil_matrix(train.shape)
-
-    if fraction:
-        try:
-            user_index = np.random.choice(
-                np.where(np.bincount(train.row) >= split_count * 2)[0],
-                replace=False,
-                size=np.int64(np.floor(fraction * train.shape[0]))
-            ).tolist()
-        except:
-            print(('Not enough users with > {} '
-                  'interactions for fraction of {}')\
-                  .format(2*split_count, fraction))
-            raise
-    else:
-        user_index = range(train.shape[0])
-
-    train = train.tolil()
-
-    for user in user_index:
-        val_test_interactions = np.random.choice(interactions.getrow(user).indices,
-                                        size=split_count,
-                                        replace=False)
-        split = np.array_split(val_test_interactions, 2)
-        val_interactions = split[0]
-        test_interactions = split[1]
-        train[user, val_test_interactions] = 0.
-        # These are just 1.0 right now
-        val[user, val_interactions] = interactions[user, val_interactions]
-        test[user, test_interactions] = interactions[user, test_interactions]
-
-
-    # Test and training are truly disjoint
-    assert(train.multiply(test).nnz == 0)
-    return train.tocsr(), val.tocsr(), test.tocsr(), user_index
 
 def subset_to_matrix(interactions, uid_to_idx, mid_to_idx, ratings, subset):
 
@@ -147,92 +100,50 @@ def subset_to_matrix(interactions, uid_to_idx, mid_to_idx, ratings, subset):
     return sub_mat.tocsr()
 
 def main():
-    fit_time = []
-    train_time = []
-    val_time = []
-    test_time = []
-    train_map = []
-    val_map = []
-    test_map = []
+    ## Read in the CSV files
+    colnames=['userId', 'movieId', 'rating', 'timestamp']
+    ratings = pd.read_csv(f'ratings_small.csv', names=colnames)
+    train_df = pd.read_csv(f'new_train_small.csv')
+    val_df = pd.read_csv(f'new_val_small.csv')
+    test_df = pd.read_csv(f'new_test_small.csv')
 
-    for i in range(5):
-        colnames=['userId', 'movieId', 'rating', 'timestamp']
-        ratings = pd.read_csv(f'ratings_small.csv', names=colnames)
-        train_df = pd.read_csv(f'new_train_small.csv')
-        val_df = pd.read_csv(f'new_val_small.csv')
-        test_df = pd.read_csv(f'new_test_small.csv')
+    train_df.reset_index(inplace=True)
 
-        train_df.reset_index(inplace=True)
+    print(ratings.head())
+    print(train_df.head())
 
-        print(ratings.head())
-        print(train_df.head())
+    ## Select only the User and Movie column that we would need
+    ratings = ratings[['userId','movieId']]
+    train_df = train_df[['userId', 'movieId']]
+    val_df = val_df[['userId', 'movieId']]
+    test_df = test_df[['userId', 'movieId']]
 
-        ratings = ratings[['userId','movieId']]
-        train_df = train_df[['userId', 'movieId']]
-        val_df = val_df[['userId', 'movieId']]
-        test_df = test_df[['userId', 'movieId']]
+    likes, uid_to_idx, idx_to_uid, mid_to_idx, idx_to_mid = df_to_matrix(ratings, 'userId', 'movieId')
 
-        likes, uid_to_idx, idx_to_uid, mid_to_idx, idx_to_mid = df_to_matrix(ratings, 'userId', 'movieId')
+    ## Obtain Sparse Matrices with interactions
+    train = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, train_df)
+    val = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, val_df)
+    test = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, test_df)
 
-        train = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, train_df)
-        val = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, val_df)
-        test = subset_to_matrix(likes, uid_to_idx, mid_to_idx, ratings, test_df)
+    assert(train.multiply(val).nnz == 0)
+    assert(train.multiply(test).nnz == 0)
+    assert(val.multiply(test).nnz == 0)
+    
+    ## Initialize model
+    model = LightFM(loss='warp')
+    
+    ## Train the model
+    model.fit(train, epochs=10)
 
-        assert(train.multiply(val).nnz == 0)
-        assert(train.multiply(test).nnz == 0)
-        assert(val.multiply(test).nnz == 0)
-        
-        start = time.process_time()
-        model = LightFM(loss='warp')
-        # Initialize model.
-        model.fit(train, epochs=10)
-        end = time.process_time()
-        elapsed_time = end - start
-        print("Time to fit model: ", elapsed_time)
-        fit_time.append(elapsed_time)
+    ## Compute Precision@K
+    train_precision = precision_at_k(model, train, k=100).mean()
+    val_precision = precision_at_k(model, val, k=100).mean()
+    test_precision = precision_at_k(model, test, k=100).mean()
 
-        start = time.process_time()
-        train_precision = precision_at_k(model, train, k=100).mean()
-        end = time.process_time()
-        elapsed_time = end - start
-        print("Time to calculate training precision: ", elapsed_time)
-        train_time.append(elapsed_time)
-
-        start = time.process_time()
-        val_precision = precision_at_k(model, val, k=100).mean()
-        end = time.process_time()
-        elapsed_time = end - start
-        print("Time to calculate validation precision: ", elapsed_time)
-        val_time.append(elapsed_time)
-
-        start = time.process_time()
-        test_precision = precision_at_k(model, test, k=100).mean()
-        end = time.process_time()
-        elapsed_time = end - start
-        print("Time to calculate test precision: ", elapsed_time)
-        test_time.append(elapsed_time)
-
-        print(train_precision)
-        print(val_precision)
-        print(test_precision)
-        train_map.append(train_precision)
-        val_map.append(val_precision)
-        test_map.append(test_precision)
-
-    plt.plot(np.arange(5), fit_time)
-    plt.show()
-    plt.plot(np.arange(5), train_time)
-    plt.show()
-    plt.plot(np.arange(5), val_time)
-    plt.show()
-    plt.plot(np.arange(5), test_time)
-    plt.show()
-    plt.plot(np.arange(5), train_map)
-    plt.show()
-    plt.plot(np.arange(5), val_map)
-    plt.show()
-    plt.plot(np.arange(5), test_map)
-    plt.show()
+    
+    print(train_precision)
+    print(val_precision)
+    print(test_precision)
 
 if __name__ == "__main__":
 
